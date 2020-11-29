14cce005911b1bb78bbd967e31716351219d1963
manuelalferez <manuelalferezruiz@gmail.com>
2020-11-29 20:24:40 +0100

    Add cookies management

--- src/server.js
+++ src/server.js
@@ -1,17 +1,41 @@
 import express from "express";
 import bodyparser from "body-parser";
+import session from "cookie-session";
 
 const server = express();
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({ extended: false }));
+server.use(
+  session({
+    maxAge: 60 * 60 * 60,
+    secret: "manuel",
+  })
+);
 
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.send(`<a href="/login">Login</a>`);
 });
 
-server.post("/", (req, res) => {
+server.get("/login", (req, res) => {
+  if (req.session.user == "abc") {
+    res.redirect("/admin");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+server.get("/admin", (req, res) => {
+  if (req.session.user === "abc") {
+    res.send("Hello admin!");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+server.post("/login", (req, res) => {
   if (req.body.user === "abc" && req.body.password === "1234") {
-    res.send("Hello admin");
+    req.session.user = req.body.user;
+    res.redirect("/admin");
   } else {
     res.send("Goodbye");
   }
